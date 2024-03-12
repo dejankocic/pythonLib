68772e227098368b63dabedeb9a4d7db2f701b25
Dejan Kocic <dejan.kocic@softqnr.com>
2024-03-12 17:28:18 +0100

    added output to file, counting loggers order by number of appearance
    output redirected to file
    counting loggers

--- pythonLib/logParse.py
+++ pythonLib/logParse.py
@@ -3,6 +3,7 @@ import re
 import zipfile
 import tempfile
 import sys
+from collections import defaultdict
 
 def unpack_zip(file_path, extract_to):
     with zipfile.ZipFile(file_path, 'r') as zip_ref:
@@ -15,7 +16,7 @@ def find_log_files_and_count_loggers(start_path):
     logger_pattern = re.compile(
         r'(?P<LogLevel>\w+)\s+(?P<Timestamp>\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{3})\s+\[(?P<Thread>[^\]]+)\]:\s(?P<Logger>[^\s]+)\s-\s(?P<Message>.+)')
 
-    loggers = {}
+    loggers = defaultdict(int)
     encodings = ['utf-8', 'utf-16-le', 'utf-16-be', 'cp1252']
 
     for root, dirs, files in os.walk(start_path):
@@ -27,8 +28,8 @@ def find_log_files_and_count_loggers(start_path):
                             for line in log_file:
                                 match = logger_pattern.search(line)
                                 if match:
-                                    logger = match.group('Logger')  # Change this line to count loggers instead of log levels
-                                    loggers[logger] = loggers.get(logger, 0) + 1
+                                    logger = match.group('Logger')
+                                    loggers[logger] += 1
                         break
                     except UnicodeDecodeError:
                         continue
@@ -41,20 +42,21 @@ def main():
         sys.exit(1)
 
     zip_files = [file for file in os.listdir(log_directory) if file.endswith('.zip')]
+    all_loggers = defaultdict(int)
 
     for zip_file in zip_files:
         print(f"Processing {zip_file}...")
         temp_dir = tempfile.mkdtemp(dir=log_directory)
         unpack_zip(os.path.join(log_directory, zip_file), temp_dir)
         loggers = find_log_files_and_count_loggers(temp_dir)
+        for logger, count in loggers.items():
+            all_loggers[logger] += count
 
-        print("Temporary directory created at:", temp_dir)
-        print("List of found loggers:")
-        for logger, count in sorted(loggers.items()):
-            print(f"{logger}: {count}")
+    with open('summary_results.txt', 'w') as f:
+        for logger, count in sorted(all_loggers.items(), key=lambda item: item[1], reverse=True):
+            f.write(f"{logger}: {count}\n")
 
-        print("Press Enter to continue to the next file.")
-        input()
+    print("Summary of found loggers has been written to 'summary_results.txt'.")
 
 if __name__ == "__main__":
     main()
